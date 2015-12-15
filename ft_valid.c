16f0c4b5eb9eb2fdbacd0c1b7af5a070c88403f5
tfolly <tfolly@student.42.fr>
2015-12-15 18:52:39 +0100

    comment sur valid

--- ft_valid.c
+++ ft_valid.c
@@ -6,12 +6,13 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 16:52:16 by tfolly            #+#    #+#             */
-/*   Updated: 2015/12/15 17:11:18 by tfolly           ###   ########.fr       */
+/*   Updated: 2015/12/15 18:52:24 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
+// doit tester pour voir si la piece rentre, il faut definir le format de la 
+// piece et celui de pos et grille avant de pouvoir la def
 int	ft_valid(char **grid, t_piece *piece, pos)
 {
 
@@ -19,7 +20,7 @@ int	ft_valid(char **grid, t_piece *piece, pos)
 
 int	ft_posvalid(char **grid, pos)
 {
-	if (grid->size >= pos)
+	if (grid->size * grid ->size >= pos)
 		return (1);
 	return (0);
 }
