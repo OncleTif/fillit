82cad262654675007ec32e0292282f9382eaf235
tfolly <tfolly@student.42.fr>
2015-12-16 16:31:15 +0100

    fix

--- ft_piecevalid.c
+++ ft_piecevalid.c
@@ -17,6 +17,7 @@ static int	ft_htouch(char *str) //verifie que les # se touchent
 		 if (str[i] == '#')
 			 if (!(str[i - 1] == '#' || str[i + 1] == '#' || str[i - 5] == '#' || str[i + 5] == '#'))
 				 return (0);
+		 i++;
 	 }
 }
 
@@ -27,7 +28,7 @@ static int	ft_nend(char *str) //verifie que les \n sont bien a la fin des lignes
 	i = 1;
 	while (i < 21)
 	{
-		if (i % 4 == 0 && str[i] != '\n')
+		if (i % 5 == 4 && str[i] != '\n')
 			return (0);
 	}
 	if (str[20] != '\n')
@@ -35,7 +36,7 @@ static int	ft_nend(char *str) //verifie que les \n sont bien a la fin des lignes
 	return (1);
 }
 
-int	ft_piecevalid(char *str)
+int			ft_piecevalid(char *str)
 {
 	int	i;
 	int	ncount;
@@ -44,7 +45,7 @@ int	ft_piecevalid(char *str)
 	i = 0;
 	ncount = 0;
 	hcount = 0;
-	while (i < 21)
+	while (i < 20)
 	{
 		if (str[i] != '.' || str[i] != '#' || str[i] != '\n')
 			return (0);
@@ -56,7 +57,5 @@ int	ft_piecevalid(char *str)
 	}
 	if (ncount != 5 || hcount != 4)
 		return (0);
-	if (ft_nend(str) && ft_htouch(str))
-		return (1);
-	return (0);
+	return (ft_nend(str) && ft_htouch(str));
 }
