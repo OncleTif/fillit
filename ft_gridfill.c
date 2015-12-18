5c1b46a3c9285738b8f6df72b67170bae1521b12
tfolly <tfolly@student.42.fr>
2015-12-18 13:16:47 +0100

    debut ftvalid

--- ft_gridfill.c
+++ ft_gridfill.c
@@ -6,24 +6,24 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 16:45:41 by tfolly            #+#    #+#             */
-/*   Updated: 2015/12/15 19:11:57 by tfolly           ###   ########.fr       */
+/*   Updated: 2015/12/18 12:59:53 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	**ft_gridfill(char **grid, t_piece *piece)
+char	**ft_gridfill(t_grid **grid, t_piece *piece)
 {
 	int pos;
 
 	pos = 0;
-	while(pos  <= grid->size)
+	while(pos <= *grid->size)
 	{
-		if (ft_valid(grid, piece, pos))
+		if (ft_valid(*grid, piece, pos))
 		{
-			ft_insert(grid, piece, pos);
+			ft_insert(*grid, piece, pos);
 			if (piece->next)
-				return (ft_gridfill(grid, piece->next));
+				return (ft_gridfill(*grid, piece->next));
 			return (grid);
 		}
 		pos++;
@@ -31,5 +31,5 @@ char	**ft_gridfill(char **grid, t_piece *piece)
 	return (0);
 }
 
-// il faut definir le type de pos
+// il faut definir le type de pos[1][1] ?
 // il faut une fct pour verifier que pos + 1 est dans grid
--- ft_valid.c
+++ ft_valid.c
@@ -6,16 +6,29 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 16:52:16 by tfolly            #+#    #+#             */
-/*   Updated: 2015/12/15 18:52:24 by tfolly           ###   ########.fr       */
+/*   Updated: 2015/12/18 13:13:03 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-// doit tester pour voir si la piece rentre, il faut definir le format de la 
-// piece et celui de pos et grille avant de pouvoir la def
-int	ft_valid(char **grid, t_piece *piece, pos)
-{
 
+int	ft_valid(t_grid **grid, t_piece *piece, int x, int y)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if (piece->xmin + x < 0 || piece->xmax + x > *grid->size
+			|| piece->ymin + x < 0 || piece->ymax + y > *grid->size)
+		return (0);
+	while (i < 4)
+	{
+		if (*grid->grid[x + piece->sq[i][0]][y + piece->sq[i][1]] != '.')
+			return (0);
+		i++;
+	}	
+	return (1);
 }
 
 int	ft_posvalid(char **grid, pos)
