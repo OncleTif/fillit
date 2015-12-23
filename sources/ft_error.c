5695f4da642b3e5867343869109896b9cb13f131
Tiphaine MANET <tmanet@e2r5p2.42.fr>
2015-12-23 12:23:12 +0100

    mise en place du message error systematique

--- sources/ft_error.c
+++ sources/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 13:54:51 by tmanet            #+#    #+#             */
-/*   Updated: 2015/12/14 13:59:44 by tmanet           ###   ########.fr       */
+/*   Updated: 2015/12/23 12:21:23 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	ft_error(char *str)
 {
-	ft_putendl(str);
-	exit(0);
+	ft_putendl("error");
+	exit(0 * (int)str);
 }
